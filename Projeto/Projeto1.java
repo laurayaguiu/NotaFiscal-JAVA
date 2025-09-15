382f45637edb198c7b93650d0fba0154b440282c
Laura Yaguiu <laurayaguiu@gmail.com>
2025-09-15 19:43:25 +0000

    esboço

--- Projeto/Projeto1.java
+++ Projeto/Projeto1.java
@@ -1,6 +1,7 @@
-public class Projrto1 {
-	
-	static void menuOpcoes() {
+import java.util.Scanner;
+public class Projeto1 {
+    // método estático q representa o menu
+    static void menuOpcoes() {
         System.out.println("""
         =========== MENU ===========
 		1. Inicializar base
@@ -15,8 +16,47 @@ public class Projrto1 {
         =============================
         """);
 	}
-	
-	public static void main (String[] args) {
-	    menuOpcoes();
-	}
+    
+    //metódos estáticos de cada opção do menu
+    static void inicializarBase() {}
+
+    static void catalogoProdutos() {}
+
+    static void adicionarItem() {}
+
+    static void resumoAtual() {}
+
+    static void finalizarVenda() {}
+
+    static void historicoVendas() {}
+
+    static void vendaEspecifica() {}
+
+    static void reporEstoque() {}
+    
+    static void relatorioEstoque() {}
+
+    // max de cada array , variável costante não dá para mudar!
+    //OBS: coloquei valores aleatórios, discutir sobre!
+    final int MaxProdutos = 45;
+    final int QuantCadaProduto = 60;
+
+    //Catálogo de produtos
+    int[] idsProdutos = new int[MaxProdutos], estoquesProdutos = new int[MaxProdutos];
+    String[] nomesProdutos = new String[MaxProdutos];
+    double[] precosProdutos = new double[MaxProdutos];
+
+    //venda atual
+    int[] vendaAtualIds = new int[MaxProdutos],  vendaAtualQuantidades = new int[QuantCadaProduto];
+
+    //Histórico de Vendas
+    int[] historicoIdsPedidos = new int[MaxProdutos];
+    double[] historicoValoresPedidos = new double[MaxProdutos];
+    int[][] historicoItensVendidos = new int[MaxProdutos][3];
+      
+    public static void main(String[] agrs) { 
+        
+        
+
+    }
 }
