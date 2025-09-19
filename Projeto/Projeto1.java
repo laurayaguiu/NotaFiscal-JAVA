f6c7835f9b3358d8359317f7affc02286e6deb38
janaina-passos <janaina.diogopassos@gmail.com>
2025-09-19 17:07:37 -0300

    Update Projeto1.java

--- Projeto/Projeto1.java
+++ Projeto/Projeto1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Projeto1 {
-    // método estático q representa o menu
-    static void menuOpcoes() {
+    // método estático que representa o menu
+      static void menuOpcoes() {   //não deveria ter um public?
         System.out.println("""
         =========== MENU ===========
 		1. Inicializar base
@@ -30,21 +30,24 @@ public class Projeto1 {
 		historicoIdsPedidos = {}
 		historicoValoresPedidos = {}
 		historicoItensVendidos = {}{3}
-		base = true;
+		base = true; //valida a inicialização da base 
 
-        System.out.print("Base iniciada com sucesso!");
+        System.out.print("Base iniciada com sucesso!"); //retornar a base para ser acessível para os outros métodos?
     }
 
     static void catalogoProdutos() {
+		//Forçar a inicialização da base 
         if(base == false){
 			System.out.println("Antes de continuar, inicialize a base!");
 		}
+
 		System.out.print("Lista de produtos com estoque positivo:")
 		System.out.println("\nID |       Produto      | Valor unitário  | Estoque ");
-        System.out.println("-----------------------------------------------");
+        System.out.println("-------------------------------------------------------");
+		//Percorre os produtos e imprime os que estão com estoque positivo
 		for (int i = 0; i < idsProdutos.length; i++) {
 			if((idsProdutos[i] != 0 && estoquesProdutos[i] > 0) {
-				System.out.printf("%d | %s | R$ %.2f | %d\n",idsProdutos[i], nomesProdutos[i], precosProdutos[i], estoquesProdutos[i]);
+				System.out.printf("%d         %s             R$ %.2f         %d\n",idsProdutos[i], nomesProdutos[i], precosProdutos[i], estoquesProdutos[i]);
 			}
 			
 		}
@@ -52,34 +55,110 @@ public class Projeto1 {
     }
 
     static void adicionarItem() {
-		 if(base == false){
+		//Forçar a inicialização da base 
+		if(base == false){
 			System.out.println("Antes de continuar, inicialize a base!");
 		} 
+		//Pedir o ID do produto e a sua quantidade
 		System.out.print("Digite o ID do produto: ");
         int id = entrada.nextInt();
         System.out.print("Digite a quantidade: ");
         int quantidade = entrada.nextInt();
+
+		//Validar se o produto existe
+
+
+		//Ver se há estoque o suficiente 
 	}
 
     static void resumoAtual() {
+		//Verifica se existe algum produto na venda atual
 		if(vendaAtualCont == 0){
 			System.out.println("Nenhum item na venda atual.");
 		}
+
+		System.out.println("\nID |     Produto    |   Quantidade  | VL. UNIT. | VL. TOTAL ");
+        System.out.println("-------------------------------------------------------------------");
+		//Listar os produtos adicionados 
 	}
 
     static void finalizarVenda() {
-		if(vendaAtualCont == 0){
+		//Verifica se existe algum produto na venda atual
+		if(vendaAtualCont == 0){ 
 			System.out.println("Nenhum item na venda atual.");
+		}
+
+		//Gera um novo ID para o pedido
+
+		//Salva o ID do pedido 
+		historicoIdsPedidos = //salvar Id
+
+		//Salva o valor total do pedido
+		historicoValoresPedidos = //salvar valor total
+			
+		//Para cada item na venda atual, adiciona uma linha na matriz historicoItensVendidos
+
+		// Debita a quantidade do estoquesProdutos
+
+		//Chama o método para imprimir a nota fiscal formatada
+		notaFiscal(//parâmentros);
+
+		//Limpa os vetores da venda atual, deixando o sistema pronto para a próxima transação
+		vendaAtualCont = 0 //não seria 2 arrays, com a quantidade e o id?
+	}
+
+	public static void notaFiscal(){
+
+		//Imprimir a nota fiscal do jeito que ele pediu -> usando printf()
+
+	}
+
+    static void historicoitensVendidos() { //matriz
+		//Percorre os vetores de histórico e imprime um resumo de todas as vendas finalizadas (ex: Pedido ID: 1001 - Valor Total: R$ 1070.50).
+		for (int id = 0; id < historicoIdsPedidos.lenght; i++){
+			
+		}
+
+		for (int v = 0; v < historicoValoresPedidos.lenght;i++){
+
+		}
+
+		//Ver como imprimir os dois juntos!!!
+	}
+
+    static void vendaEspecifica() {
+		//Solicita o ID do pedido
+		System.out.print("Digite o ID de Pedido:");
+		int idPedido = input.nextInt();
+
+		//Percorre a matriz historicoItensVendidos e encontra todos os itens do pedido
+		while (//estiver na linha do pedido, armazenar o item){
+			
+		//Reimprime a nota fiscal completa da transação 
+		notaFiscal(//parâmetros) -> chamar o método nota fiscal de novo?
 		}
 	}
 
-    static void historicoVendas() {}
+    static void reporEstoque() {
+		// Solicita um ID de produto e uma quantidade	
+		System.out.print("Digite um ID de produto:");
+		int idDigitado = input.nextInt();
 
-    static void vendaEspecifica() {}
+		System.out.print("Digite a quantidade:");
+		int quant = input.nextInt();
 
-    static void reporEstoque() {}
+		//Adiciona a quantidade no estoque no produto certo
+		
+	}
     
-    static void relatorioEstoque() {}
+    static void relatorioEstoqueBaixo() {
+		 //Imprime uma lista de todos os produtos cujo estoque esteja abaixo de um limiar pré-definido (ex: 10 unidades)
+		System.out.println("---LISTA DE PRODUTOS COM ESTOQUE ABAIXO DO LIMITE---")
+		for (int p = 0; p < estoquesProdutos.lenght; p++){
+			if (estoquesProdutos[p] < //limite){
+				System.out.println(//produto, id, quantidade talvez)
+		]
+	}
 
     // max de cada array , variável costante não dá para mudar!
     //OBS: coloquei valores aleatórios, discutir sobre!
